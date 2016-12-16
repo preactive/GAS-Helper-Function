6c39dff97ed4c6a4b17d35f1e7f5589d6ad5f4e5
Michael Messmer <preactive@gmail.com>
2016-12-15 16:03:06 -0800

    Create dupsExpose_.js

---
+++ dupsExpose_.js
@@ -0,0 +1,44 @@
+//~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`
+//--//
+// Script return Duplicates from input.  Use objToString_() to beautify output or return.
+/*
+Usage:
+  var duplicates = dupeOut["array"]
+  //  [[Robert Broeckelmann], [Chindu Thilakan]]
+  var dupesObj = dupeOut["dupCountsObj"]
+  //{name1=-1.0, name2=-1.0, name3=5.0, name4=-1.0,..............}
+*/
+
+function dupExpose_(RefRange,Offset)
+{
+  
+  if(Object.prototype.toString.call(RefRange) === '[object Array]')
+  {
+    var dataRange = RefRange;
+  }
+
+  if(Object.prototype.toString.call(RefRange) === '[object String]')
+  {
+    var dataRange = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(RefRange).getDataRange().getValues();
+  }
+  
+  var items = {};
+  var duplicates = [];
+  
+  for (var i = 1, iL = dataRange.length; i < iL ; i++) 
+  {
+    var LoopDupCheck = dataRange[i][Offset];
+    if (items[LoopDupCheck] >= 1){items[LoopDupCheck] = 1+items[LoopDupCheck];continue;}
+    if (items[LoopDupCheck] == -1)
+    {
+      duplicates.push([LoopDupCheck]);
+      items[LoopDupCheck] = 2;
+      continue;
+    }
+    items[LoopDupCheck] = -1
+  }
+  var output = {array:duplicates,dupCountsObj:items}
+  return output
+}
+
+//~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`~,~`
