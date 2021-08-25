3fa481fa06f2d62654c095ab5683fc681271cac5
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-25 19:35:30 +0800

    Delete clearSheets.js

--- clearSheets.js
+++
@@ -1,7 +0,0 @@
-function clearSheets(){
- var sheets = SpreadsheetApp.getActiveSpreadsheet().getSheets();
-  for (var i=0; i<sheets.length; i++){
-    sheets[i].clear();
-  }
-  SpreadsheetApp.flush();
-}
